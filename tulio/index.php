438db749a47e1010eb18ad7c9a51cbc6e6c96c27
Tulio20dev <marcotuliosilvaborgesdev@gmail.com>
2025-10-14 21:34:50 -0300

    Add files via upload

---
+++ tulio/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="webcss.css">
+</head>
+<body>
+    <form action="index.php" method="post">
+
+    <h1> Cadastro simples </h1>
+    
+    <span>Digite um nome:</span> <input type="text" id="nome"><br>
+    <span>Digite sua idade:</span> <input type="number" id="idade"><br>
+    <span>Digite sua altura:</span> <input type="number" id="altura"><br>
+    <button id="enviar" onclick="enviar()">enviar</button>
+
+    </form>
+
+    
+</body>
+</html>
